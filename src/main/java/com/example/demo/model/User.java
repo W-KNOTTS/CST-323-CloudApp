51fa5e0920fdd6bc6c457b39f7de4db8b9e722b7
unknown <wknotts@my.gcu.edu>
2024-03-09 13:12:36 -0700

    Fixed db add by correcting the id in db, added some building scripts including docker, launching as docker cloud app on azure

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
 
@@ -22,6 +23,12 @@ public class User
     @Column(name = "created_at")
     private Timestamp createdAt;
 
+    //please work...
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Timestamp(System.currentTimeMillis());
+    }
+    
     // Getters and setters
 
     public Long getId() 
